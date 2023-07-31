21a12e4fe75660d852626c944f3cd4b966bb2a74
SeungJin <mixkio@naver.com>
2023-07-31 15:42:36 +0900

    add deploy.sh & runserver.sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+# git clone
+echo "start to clone"
+git clone https://github.com/mixkio/lion_django_app.git
+cd lion_django_app
+
+# venv 설치
+echo "start to install venv"
+sudo apt-get update && apt intall -y python3.8-venv
+
+# venv 구성
+echo "start to make venv"
+python3 -m venv venv
+
+# 가상환경 작동
+echo "start to activate venv"
+source venv/bin/activate
+
+# pip install
+echo "start to install requirements"
+pip intall -r requirements.txt
+
+# runserver
+echo "start to runserver"
+cd lion_app && python3 manage.py runserver 0.0.0.0:8000
---
+++ runserver.sh
@@ -0,0 +1,8 @@
+# git pull
+cd ~/app/lion_django_app
+git pull origin main
+# source
+source venv/bin/activate
+# runserver
+cd lion_app
+python3 manage.py runserver 0.0.0.0:8000
